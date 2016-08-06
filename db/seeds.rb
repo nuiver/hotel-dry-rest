afe67571674dbee0fd46cd36ec68da89160c6c75
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-06 16:23:29 +0200

    Seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,38 +6,56 @@ room3 = Room.create( name_t: "luxery", price: 100, beds: 2  )
 room4 = Room.create( name_t: "suite", price: 150 , beds: 1 )
 
 booking1 = Booking.create(
-  begin_date: "2016-08-07 13:41:07 +0200",
-  end_date: "2016-08-09 13:41:07 +0200",
+  begin_date: "2016-08-07 00:00:00 +0200",
+  end_date: "2016-08-09 00:00:00 +0200",
   user_name: "Matthew",
   email: "matt@codaisseur.com",
   num_persons: 1,
   made_booking: true,
   room_id: 1
   )
-booking1 = Booking.create(
-  begin_date: "2016-08-14 13:41:07 +0200",
-  end_date: "2016-08-17 13:41:07 +0200",
+booking2 = Booking.create(
+  begin_date: "2016-08-14 00:00:00 +0200",
+  end_date: "2016-08-17 00:00:00 +0200",
   user_name: "Miriam",
   email: "miriam@codaisseur.com",
   num_persons: 1,
   made_booking: true,
   room_id: 1
   )
-booking1 = Booking.create(
-  begin_date: "2016-08-08 13:41:07 +0200",
-  end_date: "2016-08-10 13:41:07 +0200",
+booking3 = Booking.create(
+  begin_date: "2016-08-08 00:00:00 +0200",
+  end_date: "2016-08-10 00:00:00 +0200",
   user_name: "Wouter",
   email: "wouter@codaisseur.com",
-  num_persons: 2, 
+  num_persons: 2,
   made_booking: true,
   room_id: 2
   )
-booking1 = Booking.create(
-  begin_date: "2016-08-11 13:41:07 +0200",
-  end_date: "2016-08-16 13:41:07 +0200",
+booking4 = Booking.create(
+  begin_date: "2016-08-11 00:00:00 +0200",
+  end_date: "2016-08-16 00:00:00 +0200",
   user_name: "Benjamin",
   email: "benjamin@codaisseur.com",
   num_persons: 2,
   made_booking: true,
   room_id: 2
+  )
+booking5 = Booking.create(
+  begin_date: "2016-08-08 00:00:00 +0200",
+  end_date: "2016-08-16 00:00:00 +0200",
+  user_name: "Rembert",
+  email: "rembert@codaisseur.com",
+  num_persons: 1,
+  made_booking: true,
+  room_id: 3
+  )
+booking6 = Booking.create(
+  begin_date: "2016-08-17 00:00:00 +0200",
+  end_date: "2016-08-18 00:00:00 +0200",
+  user_name: "Tjin",
+  email: "tjin@codaisseur.com",
+  num_persons: 2,
+  made_booking: true,
+  room_id: 3
   )
