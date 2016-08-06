93c0d942fef35e5863ae356aa50bb36ae67444bf
RubenSloote <rubensloote@gmail.com>
2016-08-06 12:47:18 +0200

    first booking commit

---
+++ db/migrate/20160806101835_create_bookings.rb
@@ -0,0 +1,15 @@
+class CreateBookings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bookings do |t|
+      t.date :begin_date
+      t.date :end_date
+      t.string :user_name
+      t.string :email
+      t.integer :room_id
+      t.integer :num_persons
+      t.boolean :made_booking
+
+      t.timestamps
+    end
+  end
+end
